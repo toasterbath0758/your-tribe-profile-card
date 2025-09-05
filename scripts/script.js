9a6e81cdbcf269f537861a00901934e67ee7f80b
toasterbath0758 <toasterbath0758@gmail.com>
2025-09-05 09:47:31 +0200

    fixed issue
    vlak van de hover/rollover stond te hoog, staat nu op de goede plek

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ expandButton?.addEventListener('click', function() {
   /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
 })
+let audio1 = document.querySelector('#myAudio')
+ function playAudio() { 
+  audio1.play(); 
+} 
+
+function pauseAudio() { 
+  audio1.pause(); 
+} 
+
+     
