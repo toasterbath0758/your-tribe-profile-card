092829264254220ece6833ed05c0580b6cbd2806
toasterbath0758 <toasterbath0758@gmail.com>
2025-09-05 10:58:25 +0200

    update 1.5
    fixed hover?

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,16 @@ expandButton?.addEventListener('click', function() {
   /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
 })
-let audio1 = document.querySelector('#myAudio')
- function playAudio() { 
-  audio1.play(); 
-} 
+// let audio1 = document.querySelector('#myAudio') playAudio() { 
+//   audio1.play(); 
+// } 
 
 
-     
+const audio1 = document.getElementById('myAudio')
+const audio = new Audio('./assets/Monkey_sfxA.wav')
+playAudio() {
+  // audio.play();
+  console.log("test")
+}
+
+audio1.addEventListener('click', playAudio)
